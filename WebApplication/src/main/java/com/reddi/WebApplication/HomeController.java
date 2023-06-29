569fdef9ec27bc93a7d1d029dd5168cf2a980164
rspraneeth <rspraneeth.rsp@gmail.com>
2023-06-29 11:14:16 -0500

    sending data to view using ModelAndView

--- WebApplication/src/main/java/com/reddi/WebApplication/HomeController.java
+++ WebApplication/src/main/java/com/reddi/WebApplication/HomeController.java
@@ -1,20 +1,23 @@
 package com.reddi.WebApplication;
 
-import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //making it a controller
 public class HomeController {
     @RequestMapping("home") //path
     //@ResponseBody // to display returning string, if uncommented home.jsp will be displayed in browser
-    public String home(HttpServletRequest req){
-        HttpSession session = req.getSession();
-        String val = req.getParameter("name");
-        System.out.println("Home..hi "+val);
-        session.setAttribute("name", val);
-        return "home"; // returning a view, to display the view add dependency "tomcat-jasper", to support JSP.
+    public ModelAndView home(@RequestParam("name") String myName){
+        ModelAndView mv = new ModelAndView();
+        System.out.println("Home..hi "+myName);
+        mv.addObject("name", myName);
+        mv.setViewName("home");
+
+        return mv; // returning a view, to display the view add dependency "tomcat-jasper", to support JSP.
     }
 }
