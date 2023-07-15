5c8c67a58c14e07387bf79660747f8f3b70b595b
rspraneeth <rspraneeth.rsp@gmail.com>
2023-07-14 21:17:01 -0500

    SpringDataRest

---
+++ SpringDataRest/src/main/java/com/rsp/SpringDataRest/AlienRepo.java
@@ -0,0 +1,10 @@
+package com.rsp.SpringDataRest;
+
+import com.rsp.SpringDataRest.model.Alien;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "aliens", path = "aliens")
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
+
+}
---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into ALIEN values (102, 'Praneeth', 'SpringBoot');
+insert into ALIEN values (103, 'Yosha', 'SQL');
+insert into ALIEN values (104, 'Sukruthi', 'Java');
+insert into ALIEN values (101, 'Satya', 'Microservices');
+insert into ALIEN values (105, 'RSP', 'SpringBoot');
