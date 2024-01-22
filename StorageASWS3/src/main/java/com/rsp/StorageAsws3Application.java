e892e9e9017f811dc198ca940cc12dfb41880f1d
rspraneeth <rspraneeth.rsp@gmail.com>
2024-01-22 13:32:34 -0800

    upload, fetch, delete image from aws s3 storage

---
+++ StorageASWS3/src/main/java/com/rsp/StorageAsws3Application.java
@@ -0,0 +1,13 @@
+package com.rsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageAsws3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageAsws3Application.class, args);
+    }
+
+}
