3ddd6954f88bda2e4e6b75ff95d5f35cc5736758
rspraneeth <rspraneeth.rsp@gmail.com>
2024-01-27 22:36:49 -0800

    Java Mail Sender

--- MailSender/src/main/java/com/rsp/EmailSenderService.java
+++ MailSender/src/main/java/com/rsp/EmailSenderService.java
@@ -1,6 +1,7 @@
 package com.rsp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,17 @@ public class EmailSenderService {
     private JavaMailSender mailSender;
 
     public void sendEmail(String toEmail, String subject, String body){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("hashtagrsp@gmail.com");
-        message.setTo(toEmail);
-        message.setText(body);
-        message.setSubject(subject);
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("hashtagrsp@gmail.com");
+            message.setTo(toEmail);
+            message.setText(body);
+            message.setSubject(subject);
+            mailSender.send(message);
+            System.out.println("Mail sent successfully...");
+        } catch (MailSendException e){
+            System.out.println(e.getMessage());
+        }
 
-        mailSender.send(message);
-        System.out.println("Mail sent successfully...");
     }
 }
