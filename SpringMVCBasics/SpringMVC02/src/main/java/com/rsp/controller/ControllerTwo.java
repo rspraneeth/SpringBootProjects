7eee0a7c3aaf3b69e31f26be9b1db862a508481e
rspraneeth <rspraneeth.rsp@gmail.com>
2024-01-29 21:56:29 -0800

    model data

--- SpringMVCBasics/SpringMVC02/src/main/java/com/rsp/controller/ControllerTwo.java
+++ SpringMVCBasics/SpringMVC02/src/main/java/com/rsp/controller/ControllerTwo.java
@@ -3,11 +3,12 @@ package com.rsp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
 @Controller
+@RequestMapping("/c2")
 public class ControllerTwo {
 
     @GetMapping("/skill")
