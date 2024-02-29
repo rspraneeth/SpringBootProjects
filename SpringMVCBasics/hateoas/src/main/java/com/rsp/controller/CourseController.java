d6fc75d434f61b85d713fef017d98698eb898b77
rspraneeth <rspraneeth.rsp@gmail.com>
2024-02-28 21:23:18 -0800

    hateoas

---
+++ SpringMVCBasics/hateoas/src/main/java/com/rsp/controller/CourseController.java
@@ -0,0 +1,32 @@
+package com.rsp.controller;
+
+import com.rsp.pojo.Course;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/course")
+public class CourseController {
+
+    List<Course> courses = new ArrayList<>();
+
+    @GetMapping("/add")
+    public ResponseEntity<? extends Course> getCourseInfo(){
+        Course course = new Course(1, "Java", 22.99);
+        courses.add(course);
+        return new ResponseEntity<>(course, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public List<Course> getAllCourses(){
+        Course course = new Course(3, "Microservices", 20.99);
+        Course course1 = new Course(2, "Spring", 24.99);
+        courses.add(course);
+        courses.add(course1);
+        return courses;
+    }
+}
---
+++ SpringMVCBasics/hateoas/src/main/java/com/rsp/pojo/Course.java
@@ -0,0 +1,12 @@
+package com.rsp.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor @ToString
+public class Course {
+
+    private Integer id;
+    private String name;
+    private Double price;
+}
