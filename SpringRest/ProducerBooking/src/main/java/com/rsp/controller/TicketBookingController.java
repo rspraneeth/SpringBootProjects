a0cee3cf4959a46fc99fb9205f7b695ece15b03a
rspraneeth <rspraneeth.rsp@gmail.com>
2024-03-05 19:45:03 -0800

    added logger

--- SpringRest/ProducerBooking/src/main/java/com/rsp/controller/TicketBookingController.java
+++ SpringRest/ProducerBooking/src/main/java/com/rsp/controller/TicketBookingController.java
@@ -5,6 +5,8 @@ import com.rsp.model.Ticket;
 import com.rsp.service.ITicketBookingService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,15 +18,24 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "ticket booking api", description = "ticket booking controller")
 public class TicketBookingController {
 
+
+
     @Autowired
     private ITicketBookingService service;
+
+    private static final Logger logger = LoggerFactory.getLogger(TicketBookingController.class);
 
     @Operation(summary = "POST method", description = "creates a ticket for entered user, accepts passenger obj and returns ticket response")
     @PostMapping("/get-ticket")
     public ResponseEntity<Ticket> enrollPassenger(@RequestBody Passenger passenger){
 
+        logger.debug("request reached api end point in controller");
         Passenger p = service.registerPassenger(passenger);
         Ticket t = service.createTicket(p);
+
+        logger.info("Passenger obj saved");
+
+        logger.warn("check db, if values are saved");
 
         return new ResponseEntity<>(t, HttpStatus.OK);
     }
--- SpringRest/ProducerBooking/src/main/java/com/rsp/service/TicketBookingServiceImpl.java
+++ SpringRest/ProducerBooking/src/main/java/com/rsp/service/TicketBookingServiceImpl.java
@@ -4,6 +4,8 @@ import com.rsp.dao.IPassengerRepo;
 import com.rsp.dao.ITicketRepo;
 import com.rsp.model.Passenger;
 import com.rsp.model.Ticket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,8 @@ import java.util.Random;
 @Service
 public class TicketBookingServiceImpl implements ITicketBookingService{
 
+    private static final Logger logger = LoggerFactory.getLogger(TicketBookingServiceImpl.class);
+
     @Autowired
     private IPassengerRepo repo;
 
@@ -20,6 +24,7 @@ public class TicketBookingServiceImpl implements ITicketBookingService{
     private ITicketRepo ticketRepo;
     @Override
     public Passenger registerPassenger(Passenger passenger) {
+        logger.error("just checking error console");
         return repo.save(passenger);
     }
 
@@ -30,6 +35,7 @@ public class TicketBookingServiceImpl implements ITicketBookingService{
 
     @Override
     public Ticket  getTicketInfo(Integer tktNum) {
+        logger.trace("checking trace");
        return ticketRepo.findById(tktNum).orElseThrow();
     }
 
