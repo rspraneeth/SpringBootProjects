2c1ed8f8d9a6f8f0ae02fe88ad0e8f5d527b74f0
rspraneeth <rspraneeth.rsp@gmail.com>
2024-03-06 17:23:16 -0800

    tests

--- JUnit/JUnit01/src/test/java/ReverseStringTest.java
+++ JUnit/JUnit01/src/test/java/ReverseStringTest.java
@@ -6,7 +6,16 @@ class ReverseStringTest {
 
     @Test
     void reverseString() {
-
-        assertEquals("avaj", new ReverseString().reverseString("java"));
+        assertEquals("avaj", new ReverseString().reverseString("java"), "String reverse function is not working");
+        assertNotEquals("qqq", new ReverseString().reverseString("kohli"));
+        assertTrue("avaj".equals(new ReverseString().reverseString("java")));
+        assertFalse("alalala".length()<5);
     }
+
+    @Test
+    void reverseString_Supplier() {
+        assertEquals("avaj", new ReverseString().reverseString("java"), ()-> "String reverse function is not working");
+    }
+
+
 }
