8ce9f8d3933f252f71038fcfded7fc7aa7435399
rspraneeth <rspraneeth.rsp@gmail.com>
2024-03-06 20:20:13 -0800

    tests

--- JUnit/JUnit01/src/test/java/ReverseStringTest.java
+++ JUnit/JUnit01/src/test/java/ReverseStringTest.java
@@ -1,21 +1,53 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReverseStringTest {
 
+    ReverseString r;
+
+    @BeforeEach
+    void init(){
+        r=new ReverseString();
+        System.out.println("before each test");
+    }
+
+    @BeforeAll
+    static void initAll(){
+        System.out.println("before all tests");
+    }
+
     @Test
     void reverseString() {
-        assertEquals("avaj", new ReverseString().reverseString("java"), "String reverse function is not working");
-        assertNotEquals("qqq", new ReverseString().reverseString("kohli"));
-        assertTrue("avaj".equals(new ReverseString().reverseString("java")));
+        assertEquals("avaj", r.reverseString("java"), "String reverse function is not working");
+        assertNotEquals("qqq", r.reverseString("kohli"));
+        assertTrue("avaj".equals(r.reverseString("java")));
         assertFalse("alalala".length()<5);
+
     }
 
     @Test
     void reverseString_Supplier() {
-        assertEquals("avaj", new ReverseString().reverseString("java"), ()-> "String reverse function is not working");
+        assertEquals("avaj", r.reverseString("java"), ()-> "String reverse function is not working");
     }
 
+    @Test
+    void testArrays(){
+        int[] act = {1, 2, 3};
+        int[] exp = {1, 2, 3};
+        assertArrayEquals(exp, act);
+        assertTimeout(Duration.ofMillis(5), ()->{for (int i = 0; i < 1000000; i++){}});
+    }
 
+    @AfterEach
+    void destroy(){
+        System.out.println("cleanup after each test");
+    }
+
+    @AfterAll
+    static void destroyAll(){
+        System.out.println("cleanup once after all tests");
+    }
 }
