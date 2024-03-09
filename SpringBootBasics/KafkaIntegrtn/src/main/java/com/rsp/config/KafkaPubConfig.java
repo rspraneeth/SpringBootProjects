5695c594993b523ea1ba9d357712e35a65b5afb1
rspraneeth <rspraneeth.rsp@gmail.com>
2024-03-09 15:59:15 -0800

    kafka

---
+++ SpringBootBasics/KafkaIntegrtn/src/main/java/com/rsp/config/KafkaPubConfig.java
@@ -0,0 +1,30 @@
+package com.rsp.config;
+
+import com.rsp.model.Customer;
+import com.rsp.util.AppConstants;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaPubConfig {
+    /* Kafka Producer Configuration*/
+
+    @Bean
+    public ProducerFactory<String, Customer> producerFactory(){
+        /* ProducerFactory bean for producing messages to a Kafka topic */
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, AppConstants.HOST_URL); // Kafka broker to connect to
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // serializer class for Kafka message keys, message keys are expected to be strings
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class); // serializer class for Kafka message values, serializes objects to JSON format.
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+}
---
+++ SpringBootBasics/KafkaIntegrtn/src/main/java/com/rsp/model/Customer.java
@@ -0,0 +1,13 @@
+package com.rsp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Customer {
+
+    private Integer id;
+    private String name;
+    private String city;
+
+}
