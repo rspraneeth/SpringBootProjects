d71adab8e476197427873cd924b80a5f3e46eb0a
rspraneeth <rspraneeth.rsp@gmail.com>
2024-08-07 00:18:38 -0700

    microservices proj

--- Microservices/ApiGateway/src/main/java/com/rsp/MyFilter.java
+++ Microservices/ApiGateway/src/main/java/com/rsp/MyFilter.java
@@ -2,6 +2,8 @@ package com.rsp;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -11,6 +13,12 @@ import reactor.core.publisher.Mono;
 public class MyFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        ServerHttpRequest request = exchange.getRequest();
+        HttpHeaders headers = request.getHeaders();
+        headers.keySet().forEach(key -> System.out.println(key + ": " + headers.get(key)));
+
+
         System.out.println("filtering...");
         return chain.filter(exchange);
     }
